437d522fbd0677b17cad08cffa81c75cd8ff4409
DanielRSnell <hi@danielsnell.ninja>
2018-05-30 12:44:01 -0400

    update example

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Layout, Row } from 'antd';
 import MainNav from './components/nav'
 import home from './pages/home';
 import Register from './pages/register';
+import example from './pages/example';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
       <MainNav />
       <Route exact={true} path="/" component={home} />
       <Route exact={true} path="/register" component={Register} />
+      <Route exact={true} path="/example" component={example} />
       </Row>
       </BrowserRouter>
       </Layout>
---
+++ src/pages/example.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Example extends Component {
+    render() {
+        return (
+            <div className="example__profile">
+            <p className="example">
+                This is an example profile page.
+            </p>
+            </div>
+        )
+    }
+}
+
+export default Example;
